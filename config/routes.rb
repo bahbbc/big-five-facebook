31067d4c06c08b1776268385769e5bfdde82f170
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-05-03 23:53:21 -0300

    One more time a test - this one is the one

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: 'home#index', via: :get
   get 'auth/facebook', as: "auth_provider"
   get 'auth/facebook/callback', to: 'personality_questions#new'
-  match 'auth/failure', to: redirect('/')
 
   resources :personality_questions, only: [:index, :new, :create, :show]
 end
