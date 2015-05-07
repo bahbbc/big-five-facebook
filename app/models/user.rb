089f343995e542d6365a1df63b9c11346d4ca4a1
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-05-07 16:47:41 -0300

    An experiment =P

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     first_or_initialize.tap do |user|
-      user.name = auth['extra']['raw_info']['name'],
+      user.name = auth[:info][:name],
       user.email = auth['extra']['raw_info']['email'],
       user.token = auth['credentials']['token'],
       user.link = auth['extra']['raw_info']['link'],
       user.location = auth['info']['location'],
       user.expires_at = Time.at(auth['credentials']['expires_at']),
-      user.nickname = auth,
+      user.nickname = auth['info']['first_name'],
       user.gender = auth['extra']['raw_info']['gender'],
       user.save!
     end
