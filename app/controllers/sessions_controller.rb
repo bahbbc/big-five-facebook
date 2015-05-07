a4ee62d7fdc70faa49cb997fb3a18d0521e6b41d
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-05-07 16:18:39 -0300

    Fix sessionsController url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to new_personality_question
+    redirect_to new_personality_question_url
   end
 end
