0e4a42b4175fe57237f8efcebfef01addcaaf27a
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-05-07 16:58:52 -0300

    Final test?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     first_or_initialize.tap do |user|
-      user.name = auth['info']['first_name'],
-      user.email = auth['info']['email'],
-      user.token = auth['credentials']['token'],
       user.link = auth['extra']['raw_info']['link'],
+      user.token = auth['credentials']['token'],
       user.location = auth['info']['location'],
       user.expires_at = Time.at(auth['credentials']['expires_at']),
-      user.nickname = auth['info']['name'],
       user.gender = auth['extra']['raw_info']['gender'],
+      user.name = auth['info']['first_name'],
+      user.nickname = auth['info']['name'],
+      user.email = auth['info']['email']
       user.save!
     end
   end
