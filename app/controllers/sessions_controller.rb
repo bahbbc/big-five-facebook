53afa5eaeb757e77aeceabfc271126603a88e626
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-05-07 15:21:15 -0300

    Fix omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = UserPersonality.from_omniauth(env["omniauth.auth"])
+    user = UserPersonality.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to new_personality_question
   end
