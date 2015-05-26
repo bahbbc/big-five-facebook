f982b46c6f9e68529480c32ec2dff6bc2c8ce60b
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-05-26 09:02:55 -0300

    Add limits

--- app/models/user_personality.rb
+++ app/models/user_personality.rb
@@ -2,6 +2,6 @@ class UserPersonality < ActiveRecord::Base
 
   def self.koala(access_token)
     facebook = Koala::Facebook::API.new(access_token)
-    facebook.get_object("me?fields=posts{message}")
+    facebook.get_object("me?fields=posts.limit(200){message}")
   end
 end
