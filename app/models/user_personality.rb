b4891081904d69f74f824146680a88672e6e3ba7
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-05-26 09:02:16 -0300

    Started to work on pick up ser posts

--- app/models/user_personality.rb
+++ app/models/user_personality.rb
@@ -2,6 +2,6 @@ class UserPersonality < ActiveRecord::Base
 
   def self.koala(access_token)
     facebook = Koala::Facebook::API.new(access_token)
-    facebook.get_object("me?fields=name,email")
+    facebook.get_object("me?fields=posts{message}")
   end
 end
