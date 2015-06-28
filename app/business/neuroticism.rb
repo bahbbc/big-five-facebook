18799401b48ec4ed1816a04b9fb1ce78511a3ae1
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-06-28 11:16:44 -0300

    Fix neuroticism

--- app/business/neuroticism.rb
+++ app/business/neuroticism.rb
@@ -6,9 +6,9 @@ class Neuroticism
   end
 
   def personality
-    compute([ @personality_questions.seven_prestative, @personality_questions.ten_curiosity,
+    compute([@personality_questions.seven_prestative, @personality_questions.ten_curiosity,
       reverse_count(@personality_questions.fourteen_tense), reverse_count(@personality_questions.twenty_one_calm),
       reverse_count(@personality_questions.twenty_three_lazy), @personality_questions.thirty_four_calm_in_tense,
-      @personality_questions.thirty_six_friendly, @personality_questions.fourty_one_low_artistic)])
+      @personality_questions.thirty_six_friendly, @personality_questions.fourty_one_low_artistic])
   end
 end
