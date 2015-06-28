4e1e45126e4d61f69bc9eeb99b8da4ce62d10ce6
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-06-27 21:43:07 -0300

    Fix conscientiousness

--- app/business/conscientiousness.rb
+++ app/business/conscientiousness.rb
@@ -7,7 +7,7 @@ class Conscientiousness
 
   def personality
     compute([@personality_questions.four_sad, @personality_questions.six_quiet,
-      reverse_count(@personality_questions.seventeen_forgive), reverse_count(@personality_questions.ninteen_concerned)
+      reverse_count(@personality_questions.seventeen_forgive), reverse_count(@personality_questions.ninteen_concerned),
       @personality_questions.twenty_imaginative, reverse_count(@personality_questions.twenty_two_others_trust),
       @personality_questions.thirty_one_shy, @personality_questions.thirty_two_good,
       reverse_count(@personality_questions.thirty_eight_planner)])
