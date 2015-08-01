efec32cebe4b8960b1867d8c62f48dacfcd4c7ce
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-07-31 22:49:52 -0300

    Add user_police inside the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   root to: 'home#index', via: :get
-  get 'auth/facebook', as: "auth_provider"
+  get 'auth/facebook', as: 'auth_provider'
   get 'auth/facebook/callback', to: 'sessions#create'
+
+  get 'user_police', to: 'home#user_police', as: 'user_police'
 
   resources :personality_questions, only: [:index, :new, :create, :show]
 end
