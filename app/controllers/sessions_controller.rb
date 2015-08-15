b643d57e0be8d102f10bd2f40f3ccd863de0a85c
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-08-14 22:06:40 -0300

    Add token to sessions instead of id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
-    session[:user_id] = user.id
+    session[:user_token] = user.token
     redirect_to new_personality_question_url
   end
 end
