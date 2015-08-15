1107b5ce2c36624dbb5119d7a504b8e5aae3031c
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-08-14 22:53:41 -0300

    Add omniauth permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], {scope: 'user_about_me'}
+  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], {scope: 'user_about_me, user_posts, email'}
 end
