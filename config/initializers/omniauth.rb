8871a960f2647b44276efec9ab688841099ea565
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-09-03 10:28:23 -0300

    Remove test-app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 880497861999182, '1aee901be50ed025ef21d33b5e58a50f', {scope: 'user_about_me, user_posts'}
+  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], {scope: 'user_about_me, user_posts'}
 end
