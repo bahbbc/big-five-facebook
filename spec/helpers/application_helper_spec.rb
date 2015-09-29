dceb37470e1a52fc943bf90c4e9d80cc70834bfe
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-09-28 23:16:58 -0300

    Fix spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe '.trait_percentage' do
     context 'when reverse is false' do
       let(:personality) { double('Personality', extraversion: 3) }
+
       it 'returns 60' do
         expect(helper.trait_percentage(personality, 'extraversion')).to eql 60
       end
@@ -11,7 +12,8 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     context 'when reverse is true' do
       let(:personality) { double('Personality', extraversion: 3) }
-      it 'returns 60' do
+
+      it 'returns 40' do
         expect(helper.trait_percentage(personality, 'extraversion', true)).to eql 40
       end
     end
