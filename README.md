20be87d3ec4246afdc730aeafbb6520232387026
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-11-16 00:18:32 -0200

    Add columns

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # big-five-facebook
-Este projeto é um teste traduzido do big-five (add
-o link depois) com login do facebook. Ao logar com o facebook o
-usuário dá acesso aos dados e esses são salvos na applicação, junto
-com o resultado do teste.
+Este projeto é um teste traduzido do big-five (https://teste-personalidade.herokuapp.com/) com login do facebook. Ao logar com o facebook o usuário dá acesso aos dados e esses são salvos na applicação, junto com o resultado do teste.
 
-### Mais dados em breve =)
+### Para rodar o backup:
+
+ - `FileCreator.new.normalize_posts` - Normalize os posts de um usuário em um único arquivo (cada post é uma linha do csv)
+ - `FileCreator.new.user_and_class_data` - Retorna a meta-informação de um usuário mais seus dados de classe (personaliade e respostas dadas no questionário)
+
--- app/business/file_creator.rb
+++ app/business/file_creator.rb
@@ -53,7 +53,15 @@ class FileCreator
 
   def users_table_index
     %w(id location token nickname gender email name facebook_id extraversion agreeableness conscientiousness
-       neuroticism openness link)
+       neuroticism openness link, created_at, created_at, one_speaker, two_show_defects, three_exaustive_work, four_sad, five_original,
+       six_quiet, seven_prestative, eight_careless, nine_stressless, ten_curiosity, eleven_energy, twelve_fighter,
+       thirteen_confidence_worker, fourteen_tense, fifteen_thinker, sixteen_enthusiastic, seventeen_forgive,
+       eighteen_unorganized, ninteen_concerned, twenty_imaginative, twenty_one_calm, twenty_two_others_trust,
+       twenty_three_lazy, twenty_four_stable, twenty_five_inventive, twenty_six_assertive, twenty_seven_cold,
+       twenty_eight_perseverant, twenty_nine_instable, thrty_experience, thirty_one_shy, thirty_two_good,
+       thirty_three_eficient, thirty_four_calm_in_tense, thirty_five_routine, thirty_six_friendly,
+       thirty_seven_rude, thirty_eight_planner, thirty_nine_easily_nervous, fourty_like_think,
+       fourty_one_low_artistic, fourty_two_cooperative, fourty_three_distractive, fourty_four_sofisticated,)
   end
 
   def file_path(file_name)
