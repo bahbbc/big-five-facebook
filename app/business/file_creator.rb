71f6460c41c9afe394893c478d961e7f5bd6a9ce
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-04-21 16:21:15 -0300

    Minor files fixes

--- app/business/file_creator.rb
+++ app/business/file_creator.rb
@@ -20,12 +20,12 @@ class FileCreator
   def user_name_and_class
     file_name = "#{Time.zone.now}-ClassUsers.csv"
     CSV.open(file_path(file_name), 'w') do |csv|
-      csv << %w(id nome extraversion agreeableness conscientiousness neuroticism openness)
+      csv << %w(id nome extraversion agreeableness conscientiousness neuroticism openness created_at)
       User.all.each do |user|
         personality_result = user.user_personality
         next if personality_result.nil?
         csv << [user.id, user.nickname, personality_result.extraversion, personality_result.agreeableness,
-                personality_result.conscientiousness, personality_result.neuroticism, personality_result.openness]
+                personality_result.conscientiousness, personality_result.neuroticism, personality_result.openness, personality_result.created_at]
       end
     end
     dropbox_upload(file_path(file_name), file_name)
@@ -35,14 +35,9 @@ class FileCreator
     file_name = "#{Time.zone.now}-Questions.csv"
 
     CSV.open(file_path(file_name), 'w') do |csv|
-      csv << users_table_index
-      User.all.each do |user|
-        personality_result = user.user_personality
-        next if personality_result.nil?
+      csv << questions_table
         PersonalityQuestion.all.each do |question|
-          csv << [user.id, personality_result.extraversion, personality_result.agreeableness,
-                  personality_result.conscientiousness, personality_result.neuroticism, personality_result.openness,
-                  user.link, personality_result.created_at, question.id ,question.created_at, question.one_speaker, question.two_show_defects,
+          csv << [question.id ,question.created_at, question.one_speaker, question.two_show_defects,
                   question.three_exaustive_work, question.four_sad, question.five_original, question.six_quiet,
                   question.seven_prestative, question.eight_careless, question.nine_stressless, question.ten_curiosity,
                   question.eleven_energy, question.twelve_fighter, question.thirteen_confidence_worker, question.fourteen_tense,
@@ -56,7 +51,6 @@ class FileCreator
                   question.thirty_six_friendly, question.thirty_seven_rude, question.thirty_eight_planner,
                   question.thirty_nine_easily_nervous, question.fourty_like_think, question.fourty_one_low_artistic,
                   question.fourty_two_cooperative, question.fourty_three_distractive, question.fourty_four_sofisticated]
-        end
       end
     end
     dropbox_upload(file_path(file_name), file_name)
@@ -71,6 +65,7 @@ class FileCreator
         personality_result = user.user_personality
         next if personality_result.nil?
         PersonalityQuestion.all.each do |question|
+          next if question.created_at.to_s != personality_result.created_at.to_s
           csv << [user.id, user.location, user.token, user.nickname, user.gender, user.email,
                   user.name, user.facebook_id, personality_result.extraversion, personality_result.agreeableness,
                   personality_result.conscientiousness, personality_result.neuroticism, personality_result.openness,
@@ -96,9 +91,8 @@ class FileCreator
 
   private
 
-  def users_table_minor
-    %w(id location token nickname gender email name facebook_id extraversion agreeableness conscientiousness
-       neuroticism openness link personality_created_at question_id question_created_at one_speaker two_show_defects three_exaustive_work four_sad five_original
+  def questions_table
+    %w(question_id question_created_at one_speaker two_show_defects three_exaustive_work four_sad five_original
        six_quiet seven_prestative eight_careless nine_stressless ten_curiosity eleven_energy twelve_fighter
        thirteen_confidence_worker fourteen_tense fifteen_thinker sixteen_enthusiastic seventeen_forgive
        eighteen_unorganized ninteen_concerned twenty_imaginative twenty_one_calm twenty_two_others_trust
