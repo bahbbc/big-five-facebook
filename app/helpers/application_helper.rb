5f3585f508d7c87094825d194b8f4c940ec72119
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-05-07 09:53:01 -0300

    Add round to presenter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def trait_percentage(personality, trait, reverse = false)
-    trait = personality.send(trait) * 20
+    trait = (personality.send(trait) * 20).round
 
     reverse ? (100 - trait) : trait
   end
